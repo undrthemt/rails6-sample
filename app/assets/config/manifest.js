a9d005ff3e17ac06f8b7697b09e21c2be8eb18f0
m-yamashita <undrthemt@gmail.com>
2020-02-19 20:01:21 +0900

    [udpate] ホットリロードできるように webpackerの設定をdocker-composeに合わせる。

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -6,10 +6,6 @@
  * app/assets/config/manifest.jsでlink_treeかlink_directoryかlinkを用いて、stylesheet_link_tagでそのCSSを利用できるようにする（例: link my_makeup.css）
  * ビューにstylesheet_link_tagを書いてCSSをインクルードする（例: <%= stylesheet_link_tag 'my_makeup' %>）
  */
-//
-// @link https://github.com/ruby-grape/grape-swagger-rails#enabling-in-rails-6-sprokets-5
-// @link https://github.com/rails/sprockets/blob/master/UPGRADING.md#manifestjs
-// @todo sprocketsが3 -> 4になったことで、grape_swagger_railsのタグの読み込みが出来ない...。
 //= link grape_swagger_rails/application.css
 //= link grape_swagger_rails/application.js
 //= link_tree ../images
