87faf5d31ca6a2f2c67ff501f81bc3eff66782fe
m-yamashita <undrthemt@gmail.com>
2020-02-19 18:55:38 +0900

    [update] 説明を追加。不要なjQueryのrequireをコメントアウト。

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -11,7 +11,12 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-require("jquery");
+/**
+ * jQueryはapplication.jsの中で利用可能な状態になるので、jQueryをapplication.jsでrequireする必要はありません。
+ * すなわち、jQueryをapplication.jsの中で直接使う必要がない限り、実際にはjQueryをapplication.jsでrequireする必要はありません。
+ * @link https://techracho.bpsinc.jp/hachi8833/2020_01_17/85943
+ */
+// require("jquery");
 // require("bootstrap"); // bootstrap_custom.js を使います
 require("@rails/ujs").start()
 require("turbolinks").start()
@@ -21,6 +26,7 @@ require("channels")
 // @link https://medium.com/@guilhermepejon/how-to-install-bootstrap-4-3-in-a-rails-6-app-using-webpack-9eae7a6e2832
 import './bootstrap_custom.js'
 import '../stylesheets/application.scss'
+
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -2,7 +2,15 @@ const { environment } = require('@rails/webpacker')
 
 module.exports = environment
 
-// @link https://medium.com/@guilhermepejon/how-to-install-bootstrap-4-3-in-a-rails-6-app-using-webpack-9eae7a6e2832
+/**
+ * あらゆるpackでjQueryなどを利用する
+ *
+ * jQueryのような依存関係を多くのpackで使う必要が生じた場合、それらをpackごとにいちいちrequireするのは面倒です。
+ * 私好みのソリューションは、設定を変えて全packで利用可能にすることです（繰り返しますが、あくまでpack内での話であり、ビューでは使えません）。
+ * このスニペットによって、WebpackはjQueryモジュールを$という名前を介して全packに「提供」します。これは、各packの冒頭に以下を追加するのと同等です。
+ * import $ from 'jquery';
+ * @link https://techracho.bpsinc.jp/hachi8833/2020_01_17/85943
+ */
 const webpack = require('webpack')
 environment.plugins.append(
     'Provide',
